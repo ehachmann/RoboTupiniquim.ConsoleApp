5ec941f49582a2bee1c0fa470db4fcd3be30360c
ehachmann <ehachmann@hotmail.com>
2025-04-17 21:21:15 -0300

    Refatoração com modo orientado a objeto

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,79 +1,118 @@
 ﻿namespace RoboTupiniquim.ConsoleApp
 {
-    internal class Program
+
+    static class Robo
     {
+        public static int posicaoX;
+        public static int posicaoY;
+        public static int direcao;
 
-        // Implementada a versão sequencial para movimentação do robô
-
-        static void Main(string[] args)
+        public static void Explorar(char[] instrucoes)
         {
-            string[,] matriz = new string[6, 6];
-
-
-            string posicaoInicial = "1 2 N";
-            string comando = "EMEMEMEMM";
-
-            string[] coordenadasIniciais = posicaoInicial.Split(' '); // ["1", "2", "N"]
-
-            int posicaoX = Convert.ToInt32(coordenadasIniciais[0]); // 1 
-            int posicaoY = Convert.ToInt32(coordenadasIniciais[1]); // 2
-            char direcao = Convert.ToChar(coordenadasIniciais[2]); // N
-
-            char[] instrucoes = comando.ToCharArray();
-
             for (int i = 0; i < instrucoes.Length; i++)
             {
                 char instrucaoAtual = instrucoes[i];
 
                 if (instrucaoAtual == 'E')
-                {
-                    if (direcao == 'N')
-                        direcao = 'O';
-
-                    else if (direcao == 'O')
-                        direcao = 'S';
-
-                    else if (direcao == 'S')
-                        direcao = 'L';
-
-                    else if (direcao == 'L')
-                        direcao = 'N';
-
-                }
+                    VirarEsquerda();
 
                 else if (instrucaoAtual == 'D')
-                {
-                    if (direcao == 'N')
-                        direcao = 'L';
-
-                    else if (direcao == 'O')
-                        direcao = 'N';
-
-                    else if (direcao == 'S')
-                        direcao = 'O';
-
-                    else if (direcao == 'L')
-                        direcao = 'S';
-                }
+                    VirarEsquerda();
 
                 else if (instrucaoAtual == 'M')
-                { 
-                    if (direcao == 'N')
-                        posicaoY++;
-
-                    else if (direcao == 'O')
-                        posicaoX--;
-
-                    else if (direcao == 'S')
-                        posicaoY--;
-
-                    else if (direcao == 'L')
-                        posicaoX++;
-                }
+                    Mover();
             }
+        }
 
+        public static void VirarEsquerda()
+        {
+            if (direcao == 'N')
+                direcao = 'O';
+
+            else if (direcao == 'O')
+                direcao = 'S';
+
+            else if (direcao == 'S')
+                direcao = 'L';
+
+            else if (direcao == 'L')
+                direcao = 'N';
+        }
+
+        public static void VirarDireita()
+        {
+            if (direcao == 'N')
+                direcao = 'L';
+
+            else if (direcao == 'O')
+                direcao = 'N';
+
+            else if (direcao == 'S')
+                direcao = 'O';
+
+            else if (direcao == 'L')
+                direcao = 'S';
+        }
+
+        public static void Mover()
+        {
+            if (direcao == 'N')
+                posicaoY++;
+
+            else if (direcao == 'O')
+                posicaoX--;
+
+            else if (direcao == 'S')
+                posicaoY--;
+
+            else if (direcao == 'L')
+                posicaoX++;
+        }
+
+        public static void ExibirCoordenadas()
+        {
             Console.WriteLine($"Posição final do Robô: {posicaoX} {posicaoY} {direcao}");
             Console.ReadLine();
+        }
+
+    }
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string posicaoInicial = "1 2 N";
+            string comando = "EMEMEMEMM";
+
+            string[] coordenadasIniciais = posicaoInicial.Split(' '); 
+
+            Robo.posicaoX = Convert.ToInt32(coordenadasIniciais[0]); 
+            Robo.posicaoY = Convert.ToInt32(coordenadasIniciais[1]); 
+            Robo.direcao = Convert.ToChar(coordenadasIniciais[2]); 
+
+            char[] instrucoes = comando.ToCharArray();
+
+            Robo.Explorar(instrucoes);
+
+            Robo.ExibirCoordenadas();
+
+
+            string posicaoInicialDois = "3 3 L";
+            string comandoDois = "MMDMMDMDDM";
+
+            string[] coordenadasIniciaisDois = posicaoInicialDois.Split(' ');
+
+            Robo.posicaoX = Convert.ToInt32(coordenadasIniciaisDois[0]);
+            Robo.posicaoY = Convert.ToInt32(coordenadasIniciaisDois[1]);
+            Robo.direcao = Convert.ToChar(coordenadasIniciaisDois[2]);
+
+            char[] instrucoesDois = comando.ToCharArray();
+
+            Robo.Explorar(instrucoes);
+
+            Robo.ExibirCoordenadas();
 
         }
     }
